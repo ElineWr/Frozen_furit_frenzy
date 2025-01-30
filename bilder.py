5597c33714aca00eeca9de74caf60f967a51d138
satyve <152157314+satyve@users.noreply.github.com>
2025-01-30 09:03:14 +0100

    bakgrunnsbilder lagt til i bilder.py #10

---
+++ bilder.py
@@ -0,0 +1,6 @@
+import pygame as pg
+
+house = pg.image.load("assets/house.jpg").convert.alpha()
+camp = pg.image.load("assets/camp.wepb").convert.alpha()
+cave = pg.image.load("assets/cave.wepb").convert.alpha()
+coast = pg.image.load("assets/coast.wepb").convert.alpha()
