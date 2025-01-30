f589b53ea53a0b84363d8e6db88ebc2a9e8de9ac
satyve <152157314+satyve@users.noreply.github.com>
2025-01-30 09:29:51 +0100

    rettet i skrivefeil i bilder.py

--- bilder.py
+++ bilder.py
@@ -2,6 +2,6 @@ import pygame as pg
 
 # bakgrunnsbilder
 house = pg.image.load("assets/house.jpg").convert_alpha()
-camp = pg.image.load("assets/camp.wepb").convert_alpha()
-cave = pg.image.load("assets/cave.wepb").convert_alpha()
-coast = pg.image.load("assets/coast.wepb").convert_alpha()
+camp = pg.image.load("assets/camp.webp").convert_alpha()
+cave = pg.image.load("assets/cave.webo").convert_alpha()
+coast = pg.image.load("assets/coast.webp").convert_alpha()
