3d303e58bed4333ca120009f57864cd684deda33
ElineWr <elinewr@viken.no>
2025-01-30 08:42:21 +0100

    lage alle mappene #1 #2 #3 #4 #5 #6
    lager alle mappene vi vet vi trenger for nå

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame as pg
+from constants import *
+from random import randint
+
+
+# Start opp PyGame:
+pg.init()
+clock = pg.time.Clock()
+screen = pg.display.set_mode(SIZE)
+
+# Henter inn tekst, bilder, og spilleren:
+# Merk: Kan ikke laste inn font og bilder før vi har gjort pg.init:
+#from tekst import *
+#from bilder import *
+#from player import Player
+#from objekter import *
+
+
+player = Player(WIDTH/2+5, 260)
+
+
+running = True
+while running:
+
+    # Sjekk om brukeren avslutter vinduet:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    clock.tick(FPS)
+
+    # Tegner bakgrunnsbildet:
+    screen.blit(bg_image, (0, 0))
+
+    # Skriver tekst på skjermen:
+    # TODO: Skriv inn scoren som en tekst øverst på skjermen (bruk aunivers)
+
+    # Flytter og tegner spilleren:
+    player.draw(screen)
+    player.move()
+    #diamant.tegn(screen)
+
+
+    # Oppdater skjermen for å vise endringene:
+    pg.display.update()
+
+# Brukeren har avsluttet programmet, game-loopen er ferdig. Avslutt pygame:
+pg.quit()
