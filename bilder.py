dea035d2313d8841edcc6040908f695f34519307
satyve <152157314+satyve@users.noreply.github.com>
2025-01-30 09:30:39 +0100

    same

--- bilder.py
+++ bilder.py
@@ -3,5 +3,5 @@ import pygame as pg
 # bakgrunnsbilder
 house = pg.image.load("assets/house.jpg").convert_alpha()
 camp = pg.image.load("assets/camp.webp").convert_alpha()
-cave = pg.image.load("assets/cave.webo").convert_alpha()
+cave = pg.image.load("assets/cave.webp").convert_alpha()
 coast = pg.image.load("assets/coast.webp").convert_alpha()
