265485f74a068dd58c0549accc2ae05fa35cf4a3
ElineWr <elinewr@viken.no>
2025-02-06 19:23:07 +0100

    jobbet med aninmasjonen av victor #18
    ting å jobbe med
    - gjøre om slik at fler ting er i player.pg
    - rydde opp
    - fikse feil som gjør at man plutselig går ut av spillet
    - gjøre at man kan bevege seg + animasjon
    - sikkert mer

--- bilder.py
+++ bilder.py
@@ -12,5 +12,5 @@ camp = pg.transform.scale(camp, SIZE)
 cave = pg.transform.scale(cave, SIZE)
 coast = pg.transform.scale(coast, SIZE)
 
-victor = pg.image.load("assets/victor_sprites-removebg-preview.png").convert_alpha()
-victor = pg.transform.scale(victor, (541, 115))
+victor = pg.image.load("assets/victor-removebg-preview.png").convert_alpha()
+victor = pg.transform.scale(victor, (629, 75))
--- main.py
+++ main.py
@@ -20,13 +20,32 @@ player = Player(x = 700, y = 220, dy = 1, image = victor, money = 0, carryingFoo
 
 sprite_sheet = SpriteSheet(victor)
 
-frame_0 = sprite_sheet.get_image( frame = 0,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
-frame_1 = sprite_sheet.get_image( frame = 1,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
-frame_2 = sprite_sheet.get_image( frame = 2,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
-frame_3 = sprite_sheet.get_image( frame = 3,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
-frame_4 = sprite_sheet.get_image( frame = 4,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
-frame_5 = sprite_sheet.get_image( frame = 5,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+# lager animasjons listen
 
+animasjons_liste = []
+animasjon_steps = [6, 3]  
+handling = 0
+siste_oppdadering = pg.time.get_ticks()
+animasjon_cooldown = 500
+frame = 0
+steps_teller = 0
+
+for animasjons in animasjon_steps:
+    midlertidig_bilde_liste = [] 
+    for _ in range(animasjons):
+        midlertidig_bilde_liste.append(sprite_sheet.get_image(steps_teller, 40, 75, 1.5, BLACK))
+        steps_teller += 1
+    animasjons_liste.append(midlertidig_bilde_liste)
+
+
+    """
+    frame_0 = sprite_sheet.get_image( frame = 0,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+    frame_1 = sprite_sheet.get_image( frame = 1,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+    frame_2 = sprite_sheet.get_image( frame = 2,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+    frame_3 = sprite_sheet.get_image( frame = 3,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+    frame_4 = sprite_sheet.get_image( frame = 4,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+    frame_5 = sprite_sheet.get_image( frame = 5,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+    """
 
 
 
@@ -37,17 +56,43 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_DOWN and handling > 0:
+                handling -= 1
+                fram = 0
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_UP and handling < len(animasjons_liste) - 1:
+                handling += 1
+                fram = 0
 
     clock.tick(FPS)
 
     # Tegner bakgrunnsbildet:
     screen.blit(house, (0, 0))
 
+    # Oppdader animasjonen
+    naatid = pg.time.get_ticks()
+    if naatid - siste_oppdadering >= animasjon_cooldown:
+        frame += 1
+        siste_oppdadering = naatid
+        if frame >= len(animasjons_liste[handling]):
+            frame = 0
+
+    screen.blit(animasjons_liste[handling][frame], (0, 0))
+
+
+
+    """
+    for x in range(animasjon_steps):
+        screen.blit(animasjons_liste[x], (x*72, 0))
+    """
+    """
     screen.blit(frame_0, (0, 0))
     screen.blit(frame_1, (100, 0))
     screen.blit(frame_2, (200, 0))
     screen.blit(frame_3, (300, 0))
     screen.blit(frame_4, (400, 0))
+    """
 
     # # Skriver tekst på skjermen:
     # # TODO: Skriv inn scoren som en tekst øverst på skjermen (bruk aunivers)
