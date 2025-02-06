d9dc53beacb141de0be0561f21938fbb80d33100
ElineWr <elinewr@viken.no>
2025-02-06 17:24:48 +0100

    loader inn alle løpe sprites
    skal se på animasjon nå <:

--- main.py
+++ main.py
@@ -12,28 +12,22 @@ screen = pg.display.set_mode(SIZE)
 # Merk: Kan ikke laste inn font og bilder før vi har gjort pg.init:
 #from tekst import *
 from bilder import *
-from player import Player
+from player import Player, SpriteSheet
 from objekter import *
 
 
 player = Player(x = 700, y = 220, dy = 1, image = victor, money = 0, carryingFood = False)  #WIDTH/2+5, 260)
 
-def get_image(sheet, frame, width_sprite, height_sprite, scale, color):
-    image = pg.Surface((width_sprite, height_sprite)).convert_alpha()
-    image.blit(sheet, (0, 0), ((frame * width_sprite), 0, width_sprite, height_sprite))
-    image = pg.transform.scale(image, (width_sprite * scale, height_sprite * scale))
-    image.set_colorkey(color)
+sprite_sheet = SpriteSheet(victor)
 
-    return image
+frame_0 = sprite_sheet.get_image( frame = 0,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+frame_1 = sprite_sheet.get_image( frame = 1,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+frame_2 = sprite_sheet.get_image( frame = 2,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+frame_3 = sprite_sheet.get_image( frame = 3,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+frame_4 = sprite_sheet.get_image( frame = 4,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
+frame_5 = sprite_sheet.get_image( frame = 5,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
 
-frame_0 = get_image(victor, frame = 0,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
-frame_1 = get_image(victor, frame = 1,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
-frame_2 = get_image(victor, frame = 2,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
-frame_3 = get_image(victor, frame = 3,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
-frame_4 = get_image(victor, frame = 4,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
-frame_5 = get_image(victor, frame = 5,  width_sprite = 90, height_sprite = 115, scale = 1, color = BLACK)
 
-    
 
 
 running = True
@@ -50,6 +44,10 @@ while running:
     screen.blit(house, (0, 0))
 
     screen.blit(frame_0, (0, 0))
+    screen.blit(frame_1, (100, 0))
+    screen.blit(frame_2, (200, 0))
+    screen.blit(frame_3, (300, 0))
+    screen.blit(frame_4, (400, 0))
 
     # # Skriver tekst på skjermen:
     # # TODO: Skriv inn scoren som en tekst øverst på skjermen (bruk aunivers)
--- player.py
+++ player.py
@@ -4,6 +4,20 @@ from constants import PLAYER_SPEED, WIDTH, HEIGHT
 from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT)
 from bilder import *
 
+class SpriteSheet():
+    def __init__(self, image):
+        self.sheet = image
+    
+    def get_image(self, frame, width_sprite, height_sprite, scale, color):
+        image = pg.Surface((width_sprite, height_sprite)).convert_alpha()
+        image.blit(self.sheet, (0, 0), ((frame * width_sprite), 0, width_sprite, height_sprite))
+        image = pg.transform.scale(image, (width_sprite * scale, height_sprite * scale))
+        image.set_colorkey(color)
+
+        return image
+
+
+
 class Player(Object):
 
     def __init__(self, x, y, dy, image, money, carryingFood):
