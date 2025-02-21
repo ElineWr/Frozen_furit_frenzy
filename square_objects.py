aa2011c09b7fa8d260ed8d43dcd06c748084da4e
satyve <152157314+satyve@users.noreply.github.com>
2025-02-21 19:09:54 +0100

    Blitt kvitt potensielle farer for Victor og justert hvor han kan gå. #20

--- square_objects.py
+++ square_objects.py
@@ -16,9 +16,9 @@ class Square(pg.sprite.Sprite):
 
     
     def tegn(self, screen, game):  # Legg til current_background som parameter
-        pass
-        # if self.background == game.current_background_index:
-        #      pg.draw.rect(screen, self.farge, (self.x, self.y, self.width, self.height))
+        # pass
+         if self.background == game.current_background_index:
+              pg.draw.rect(screen, self.farge, (self.x, self.y, self.width, self.height))
             
     # def update(self): 
     #     self.y += 5
@@ -47,7 +47,7 @@ class Square(pg.sprite.Sprite):
         
 all_squares = [
     # house 
-    Square(farge=BLACK, x=0, y=0, width=250, height=HEIGHT, background=0),
+    Square(farge = BLACK, x = 0, y = 0, width = 250, height = HEIGHT, background=0),
     Square(farge = BLACK, x = WIDTH - 50, y = 0, width = 50, height = (HEIGHT*(4/8)), background = 0),
     Square(farge = BLACK, x = WIDTH - 50, y = (HEIGHT*(6/8)), width = 50, height = (HEIGHT*(2/8)), background = 0),
     Square(farge = BLACK, x = 0, y = 0, width = WIDTH, height = (HEIGHT*(1.5/8)), background = 0),
@@ -55,14 +55,16 @@ all_squares = [
     
     # Square(farge = BLACK, x = 0, y = HEIGHT - , width = 50, height = (HEIGHT*(4/8)), background = 0),
     # coast
-    Square(farge=RED, x=0, y=0, width=WIDTH*(0.5/10), height=HEIGHT*(5/10), background=1),
+    Square(farge = RED, x=0, y=0, width=WIDTH*(0.5/10), height=HEIGHT*(5/10), background=1),
     Square(farge = RED, x = 0, y = (HEIGHT*(6/8)), width = 50, height = (HEIGHT*(2/8)), background = 1),
+    Square(farge = RED, x = 0, y = 0, width=350, height=50, background=1),
     
     
     # camp
     Square(farge = BLACK, x = 0, y = 0, width = WIDTH*(6/10), height= 50, background = 2),
     Square(farge = BLACK, x = 0, y = HEIGHT*(7/10), width = 150, height=150, background = 2),
     Square(farge = BLACK, x = 250, y = HEIGHT*(8/10), width = 100, height = 100, background = 2),
+    Square(farge = BLACK, x = 0, y = HEIGHT - 50, width = 300, height = 50, background=2),
     
     # coast
     Square(farge = BLACK, x = 0, y = 0, width=150, height=300, background=3),
@@ -71,6 +73,11 @@ all_squares = [
     Square(farge = BLACK, x = WIDTH*(5/10), y = HEIGHT*(2/10), width=200, height=260, background=3),
     Square(farge = BLACK, x = 0, y = 0, width = WIDTH*(4.5/10), height=75, background=3),
     Square(farge = BLACK, x = 0, y = HEIGHT-100, width=WIDTH*(5/10), height=100, background=3),
+    Square(farge = BLACK, x = WIDTH - 200, y = 0, width=200, height=75, background=3),
+    Square(farge = BLACK, x = WIDTH - 300, y = 0, width= 300, height= 50, background=3),
+    Square(farge = BLACK, x = WIDTH - 175, y = HEIGHT*(5.5/10), width=175, height=175, background=3),
+    Square(farge = BLACK, x = WIDTH - 50, y = HEIGHT*(1/10), width=50, height = 300, background=3)
+    
 
 ]
 squares = pg.sprite.Group()
